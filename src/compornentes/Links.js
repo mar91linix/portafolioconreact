6bc9cada14aaec4033997ef57a89a2b8b5cef28d
Your Name <you@example.com>
2020-12-03 19:13:57 -0500

    primera version

---
+++ src/compornentes/Links.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+export const Links = () => {
+      return (
+        <div>
+           <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Proyectos <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Descripción</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      
+    </ul>
+  </div> 
+        </div>
+    )
+}
+
+
+
---
+++ src/container/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Links } from '../compornentes/Links'
+
+
+ export const NavBar = () => {
+    return (
+        <>
+        <nav className ="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Lina Marcela Zapata Bernal</a>
+ 
+
+  <Links/>
+</nav>
+        </>
+    )
+}
+
+
+
+
+ 
